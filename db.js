315bb882f966a774905221957ce1a3ee2eacbd63
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-12-11 11:36:27 -0300

    added more artists to db

--- db.js
+++ db.js
@@ -4,22 +4,204 @@ module.exports = {
       name: "Cielos de Plomo",
       description:
         "Cielos de Plomo es un proyecto musical en mutación, nacido en Maldonado, Uruguay y actualmente basado en Montevideo.",
-      image: "Cielos-de-Plomo/Cielos-de-Plomo",
+      image: "Cielos de Plomo",
       albums: [],
     },
     {
       name: "Mux",
       description:
         "Mux es un sexteto que une la música instrumental y las canciones de una forma particular. Se formó en montevideo en 2008",
-      image: "Mux/Mux",
+      image: "Mux",
       albums: [],
     },
     {
       name: "Bolsa de naylon en la rama de un arbol",
       description:
-        "A medio camino entre la tradición y eso otro que algunos llaman innovación (pero nosotros no) la música de Bolsa está llena de dudas, decisiones tomadas a partir de errores, cosas pautadas e improvisiación. Música instrumental con alguna intervención vocal en donde el candombe convive con el drum n bass, la plena con el free jazz, las raices con el glitch, el humor con el amor.",
-      image:
-        "Bolsa-de-naylon-en-la-rama-de-un-arbol/Bolsa-de-naylon-en-la-rama-de-un-arbol",
+        "A medio camino entre la tradición y eso otro que algunos llaman innovación (pero nosotros no) la música de Bolsa está llena de dudas, decisiones tomadas a partir de errores, cosas pautadas e improvisación. Música instrumental con alguna intervención vocal en donde el candombe convive con el drum n bass, la plena con el free jazz, las raices con el glitch, el humor con el amor.",
+      image: "Bolsa de naylon en la rama de un arbol",
+      albums: [],
+    },
+    {
+      name: "Solar",
+      description: "",
+      image: "Solar",
+      albums: [],
+    },
+    {
+      name: "El Dúo Melódico",
+      description: "",
+      image: "El Dúo Melódico",
+      albums: [],
+    },
+    {
+      name: "Hey Ladies",
+      description: "",
+      image: "Hey Ladies",
+      albums: [],
+    },
+    {
+      name: "Martin Barea Mattos y X hora X dia X mes",
+      description: "",
+      image: "Martin Barea Mattos y X hora X dia X mes",
+      albums: [],
+    },
+    {
+      name: "Uoh!",
+      description:
+        "Uoh! es una banda mayormente instrumental de Montevideo. Hay mucha improvisación; de guitarras tranquilas, a partes pesadas pasando por partes bailables y bardo.",
+      image: "Uoh!",
+      albums: [],
+    },
+    {
+      name: "Alessandro Podestá",
+      description:
+        "Lleva 7 discos editados: Aspavento (2009), medias tintas (2010), lo que no sé (2012), Partido el ganado (2014), Más Quereres (2017), de la piedra de allá (2019) y Viento diablito (2019), participa en dos antologías de La Lupa Libros (2010 y 2011) y discos de colegas como Valentina Pecora, Javier Alves y Zelma Vargas. ",
+      image: "Alessandro Podestá",
+      albums: [],
+    },
+    {
+      name: "Florida",
+      description: "",
+      image: "Florida",
+      albums: [],
+    },
+    {
+      name: "Genuflexos",
+      description: "",
+      image: "Genuflexos",
+      albums: [],
+    },
+    {
+      name: "Abdul",
+      description: "",
+      image: "Abdul",
+      albums: [],
+    },
+    {
+      name: "Tangente",
+      description:
+        "Tangente son: Mauricio Ramos; batería y percusión Juan Martín López; guitarra y voz, Nacho Correa; Bajo y contrabajo",
+      image: "Tangente",
+      albums: [],
+    },
+    {
+      name: "Pancho",
+      description: "",
+      image: "Pancho",
+      albums: [],
+    },
+    {
+      name: "Pacientes",
+      description: "",
+      image: "Pacientes",
+      albums: [],
+    },
+    {
+      name: "Santo Dubois",
+      description: "",
+      image: "Santo Dubois",
+      albums: [],
+    },
+    {
+      name: "ChINO",
+      description:
+        "ChINO es un dúo de música experimental integrado por Juan Sacco en bajo, loops y vocales, y Martín Recto en batería y chapas. ChINO se basa en la repetición maquinal como materia prima de expresión.",
+      image: "ChINO",
+      albums: [],
+    },
+    {
+      name: "Strudl Von Disphonic Orchestra",
+      description: "",
+      image: "Strudl Von Disphonic Orchestra",
+      albums: [],
+    },
+    {
+      name: "Ataque Chino",
+      description:
+        "Busca integrar música y poesía en una misma experiencia estética, estableciendo relaciones y concordancias entre los textos y algunas de las posibles sonoridades que estos sugieren.",
+      image: "Ataque Chino",
+      albums: [],
+    },
+    {
+      name: "Señor Faraón",
+      description:
+        "Señor Faraón es el experimento en solitario de Ismael Varela (Montevideo, Uruguay, 1986). Edita Siguiendo Al Rayo (2009), Las Últimas en Inglés (2010) y Piel de Culebra (2015). Compone la música de las películas Nunchaku (2011) y Clever (2015). Se presenta en Uruguay, Argentina, Chile y Estados Unidos en el festival SXSW 2014. Siempre convencido de que la Música manda.",
+      image: "Señor Faraón",
+      albums: [],
+    },
+    {
+      name: "Portillo",
+      description: "",
+      image: "Portillo",
+      albums: [],
+    },
+    {
+      name: "O'neill",
+      description: "",
+      image: "O'neill",
+      albums: [],
+    },
+    {
+      name: "Carmen Sandiego",
+      description: "",
+      image: "Carmen Sandiego",
+      albums: [],
+    },
+    {
+      name: "Música Para Viajes Interdepartamentales",
+      description: "",
+      image: "Música Para Viajes Interdepartamentales",
+      albums: [],
+    },
+    {
+      name: "Patricia Turnes",
+      description: "",
+      image: "Patricia Turnes",
+      albums: [],
+    },
+    {
+      name: "Rocoto",
+      description:
+        "Desde sus inicios en 2015 Rocoto es una banda en transición constante, tanto en su integración como en su sonido. El disco “Tanto y no” fue la culminación de la primera etapa de la banda y en él participaron: Juan Manuel Varela y Alessandro Podestá -quienes continúan- y Gerardo Martínez y Javier Alves. El año 2017 tiene la inclusión de Guzmán Rivero y Gonzalo Riso.",
+      image: "Rocoto",
+      albums: [],
+    },
+    {
+      name: "Diego Cotelo",
+      description:
+        'Inquieto y de corazón ecléctico, a Diego Cotelo le resulta raro escribir de si mismo en tercera persona. Tiene varios discos "solistas" con distintos seudónimos, entidades, cada uno hecho de maneras distintas con distintas personas. Componiendo y grabando al mismo tiempo, musicalizando la palabra hablada, arreglando músicas populares de maneras atrevida, de todo un poco.',
+      image: "Diego Cotelo",
+      albums: [],
+    },
+    {
+      name: "Mena",
+      description: "",
+      image: "Mena",
+      albums: [],
+    },
+    {
+      name: "Isla de Flores",
+      description: "",
+      image: "Isla de Flores",
+      albums: [],
+    },
+    {
+      name: "Monkelis",
+      description:
+        "Monkelis está compuesto por Santiago Guidotti, Eugenio Amen y Carolina Zunino. Dos guitarras y dos tambores. Rock primitivo, divertido y elegante. Letras en inglés, español, portugués e italiano. Gritos honestos, instrumentos baratos, corazones caros.",
+      image: "Monkelis",
+      albums: [],
+    },
+    {
+      name: "Feel de Agua",
+      description: "",
+      image: "Feel de Agua",
+      albums: [],
+    },
+    {
+      name: "Excelentes Nadadores",
+      description: "",
+      image: "Excelentes Nadadores",
       albums: [],
     },
   ],
