84acad61459c6caaa009b5f0a5107dadb680237e
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-12-15 13:17:13 -0300

    all working

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,28 +2,10 @@ const { User } = require("../models");
 const createToken = require("../utils/token");
 
 module.exports = {
-  addUser: async (req, res) => {
-    const admin = await User.findById(req.user);
-    if (admin === true) {
-      const user = await new User({
-        name: req.body.name,
-        password: req.body.password,
-      });
-      user.tokens = [createToken(user.id)];
-      await user.save();
-      res.json({
-        name: user.name,
-        token: user.tokens[0],
-      });
-    } else {
-      res.json("unauthorized");
-    }
-  },
-
   logIn: async (req, res) => {
     try {
-      const user = await User.findOne({ email: req.body.email });
-      if (!user) {
+      const user = await User.findOne({ name: req.body.name });
+      if (user === null) {
         return res.status(401).json({ message: "Invalid credentials." });
       }
       if (!(await user.validPassword(req.body.password))) {
@@ -44,10 +26,25 @@ module.exports = {
     }
   },
 
+  addUser: async (req, res) => {
+    const admin = await User.findById(req.user);
+    if (admin !== null) {
+      const user = await new User({
+        name: req.body.name,
+        password: req.body.password,
+      });
+      user.tokens = [createToken(user.id)];
+      await user.save();
+      res.json("user created");
+    } else {
+      res.json("unauthorized");
+    }
+  },
+
   updateUser: async (req, res) => {
     const admin = await User.findById(req.user);
     if (admin !== null) {
-      const user = await User.findByIdAndUpdate(req.body._id, req.body, {
+      const user = await User.findByIdAndUpdate(req.user, req.body, {
         new: true,
       });
       res.json("user updated");
@@ -59,7 +56,7 @@ module.exports = {
   deleteUser: async (req, res) => {
     const admin = await User.findById(req.user);
     if (admin !== null) {
-      const users = await User.findByIdAndDelete(req.body._id);
+      const users = await User.findByIdAndDelete(req.user);
       res.json("user deleted");
     } else {
       res.json("unauthorized");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,22 @@
 const jwt = require("express-jwt");
-const { validate } = require("../validation");
+const { validate } = require("../utils/validation");
 
 const {
-  addUser,
   logIn,
+  addUser,
   updateUser,
   deleteUser,
 } = require("../controllers/userController");
 
 function userRoutes(app) {
-  app.post("/api/v1/users/addUser", validate, addUser);
-
   app.post("/api/v1/users/login", validate, logIn);
+
+  app.post(
+    "/api/v1/users",
+    validate,
+    jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
+    addUser
+  );
 
   app.put(
     "/api/v1/users",
