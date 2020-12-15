7ea65a6e0200a2c8d0ea5bb681f243320714a66f
nandopeyton <68226901+nandopeyton@users.noreply.github.com>
2020-12-15 17:11:52 -0300

    added static

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const seederRoutes = require("./routes/seederRoutes");
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
